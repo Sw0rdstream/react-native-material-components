89ad669d69d9d9e33df06eef54d4e26430e6a54a
su hang <su_h@worksap.co.jp>
2017-04-25 21:35:21 +0800

    add example ios project for development

---
+++ podExProject/podExProject/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  podExProject
+//
+//  Created by 蘇航 on 2017/4/25.
+//  Copyright © 2017年 swordstream. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+

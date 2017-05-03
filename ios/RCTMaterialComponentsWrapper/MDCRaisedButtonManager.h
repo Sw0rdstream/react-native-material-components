f90fe49f8dc6cf1bdd0cb336108e0fdad04ba82b
su hang <su_h@worksap.co.jp>
2017-05-03 15:38:40 +0800

    add MDCraisedButtonManager

---
+++ ios/RCTMaterialComponentsWrapper/MDCRaisedButtonManager.h
@@ -0,0 +1,10 @@
+//
+//  React Native Bridger to MDCRaisedButton
+//  Created by Sw0rdstream (su_h@worksap.co.jp) on 2017/4/21.
+//
+
+#import <React/RCTViewManager.h>
+
+@interface MDCRaisedButtonManager : RCTViewManager
+
+@end
